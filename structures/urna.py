8bd4e9b469826f072d7b27db5c5db6fb759127b3
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-07-22 17:14:41 -0300

    fix(encoding): utf8

--- structures/urna.py
+++ structures/urna.py
@@ -13,9 +13,9 @@ class UrnaEletronica:
     def __init__(self, partidos_csv: str, cargos_csv: str, candidatos_csv: str) -> None:
         self.cargos: list[Cargo] = []
         self.cpfs: set[str] = set()
-        self.cargos = [Cargo(linha[0], int(linha[1])) for linha in csv.reader(open(cargos_csv))]
-        self.partidos = [Partido(*linha[:3]) for linha in csv.reader(open(partidos_csv)) if len(linha) > 2]
-        for linha in csv.reader(open(candidatos_csv)):
+        self.cargos = [Cargo(linha[0], int(linha[1])) for linha in csv.reader(open(cargos_csv, encoding='utf-8'))]
+        self.partidos = [Partido(*linha[:3]) for linha in csv.reader(open(partidos_csv, encoding='utf-8')) if len(linha) > 2]
+        for linha in csv.reader(open(candidatos_csv, encoding='utf-8')):
             if len(linha) < 3:
                 continue
 
